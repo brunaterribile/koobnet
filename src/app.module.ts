e2acc0e5eec18f1603f2e641f813e80316ec93fc
Bruna Terribile <brunaterribile@gmail.com>
2023-08-22 23:33:03 -0300

    chore: create auth files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { PostsModule } from './modules/posts/posts.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [UsersModule, PostsModule, AuthModule],
   controllers: [],
   providers: [],
 })
